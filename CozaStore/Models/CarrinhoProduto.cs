65ea75ddb2b46de0d2d66e7f463a843388d95102
gynna2006 <giovannacolonisio@gmail.com>
2023-10-19 11:34:33 -0300

    Finalizacao da camada de dados

---
+++ CozaStore/Models/CarrinhoProduto.cs
@@ -0,0 +1,34 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CozaStore.Models;
+
+    [Table("CarrinhoProduto")]
+    public class CarrinhoProduto
+    {
+        [Key, Column(Order = 1)]
+        [Display(Name ="Carrinho")]
+        public int CarrinhoId { get; set; }
+        [ForeignKey("CarrinhoId")]
+        public Carrinho Carrinho { get; set; }
+
+        [Key, Column(Order = 2)]
+        [Display(Name ="Produto")]
+        public int ProdutoEstoqueId { get; set; }
+        [ForeignKey("ProdutoEstoqueId")]
+        public ProdutoEstoque ProdutoEstoque { get; set; }
+
+        [Display(Name = "Preço")]
+        [Column(TypeName = "decimal(8,2)")]
+        [Required(ErrorMessage = "Informe o Preço de Venda")]        
+        public string Preco { get; set; }
+
+        [Display(Name = "Preço com Desconto")]
+        [Column(TypeName = "decimal(8,2)")]
+        [Required(ErrorMessage = "Informe o Preço com Desconto")]        
+        public string PrecoDesconto { get; set; }
+
+        [Display(Name = "Qtde")]
+        [Required(ErrorMessage = "Informe a Qtde")]        
+        public int Qtde { get; set; }
+    }
---
+++ CozaStore/Models/ProdutoEstoque.cs
@@ -0,0 +1,44 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CozaStore.Models;
+
+    [Table("ProdutoEstoque")]
+    public class ProdutoEstoque
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Display(Name = "Produto")]
+        [Required(ErrorMessage = "Informe o Produto")]
+        public int ProdutoId { get; set; }
+        [ForeignKey("ProdutoId")]
+        public Produto Produto { get; set; }
+
+        [Display(Name = "Cor")]
+        [Required(ErrorMessage = "Informe a Cor")]
+        public byte CorId { get; set; }
+        [ForeignKey("CorId")]
+        public Cor Cor { get; set; }
+        
+        [Display(Name = "Tamanho")]
+        [Required(ErrorMessage = "Informe o Tamanho")]
+        public byte TamanhoId { get; set; }
+        [ForeignKey("TamanhoId")]
+        public Tamanho Tamanho { get; set; }
+
+        [Display(Name = "Preço")]
+        [Column(TypeName = "decimal(8,2)")]
+        [Required(ErrorMessage = "Informe o Preço de Venda")]        
+        public string Preco { get; set; }
+
+        [Display(Name = "Preço com Desconto")]
+        [Column(TypeName = "decimal(8,2)")]
+        [Required(ErrorMessage = "Informe o Preço com Desconto")]        
+        public string PrecoDesconto { get; set; }
+
+        [Display(Name = "Qtde em Estoque")]
+        [Required(ErrorMessage = "Informe a Qtde em Estoque")]        
+        public int QtdeEstoque { get; set; }
+    }
